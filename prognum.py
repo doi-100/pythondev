799b317bbc32c0d8d38097359f21d6bc6827e562
doi-100 <r_doi004@jbx.jtb.jp>
2026-05-07 18:49:07 +0900

    再帰的な関数を利用してフィボナッチ数列を実装しようの課題完了

--- prognum.py
+++ prognum.py
@@ -1,14 +1,5 @@
 def Fibo(SecNo):
-    a=1
-    b=1
-    ret =0
-    if SecNo == 1:
-        return 0
-    elif SecNo == 2:
+    if SecNo <=2 :
         return 1
     else:
-        for i in range(2,SecNo):
-            ret = a + b
-            a=b
-            b=ret
-    return ret
+        return Fibo(SecNo-1)+Fibo(SecNo-2)
